10350bcfa1c4862c539ea003d981ebabced20a8b
eshangin <shangin.jekon@mail.ru>
2014-09-14 20:57:05 +0700

    Composite Launch - start
    Start creating Composite Launch Eclipse plugin which will be able to run
    multiple launch configurations.
    
    In this commit: basic structure of new launcher

---
+++ ru.eshangin.CompositeLaunch/src/ru/eshangin/compositelaunch/CompositeLaunchConfigurationTabGroup.java
@@ -0,0 +1,16 @@
+package ru.eshangin.compositelaunch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+public class CompositeLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] { new CommonTab() }; 
+		setTabs(tabs);
+	}
+
+}
